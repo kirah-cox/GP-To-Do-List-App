c15e79e6b83dc8cbfe0f940dc728b0f0241f3051
brooklyn-p-m <brooklyn.p.merrick@gmail.com>
2026-01-26 11:17:33 -0700

    Added Tasks and Todolist

---
+++ GP To-Do List App/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace GP_To_Do_List_App
+{
+    public class Task
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string DueDate { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public Task(string name, string description, string dueDate)
+        {
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+        }
+    }
+
+
+  
+
+
+}
---
+++ GP To-Do List App/ToDoList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GP_To_Do_List_App
+{
+    public class ToDoList
+    {
+        List<Task> taskList = new List<Task>
+        {
+           //new Task {Name = "Make bed", Description = "do it", DueDate = "12/12/12", IsCompleted = false}
+        };
+        public void AddTask(string name, string description, string dueDate)
+        {
+            Task task = new Task(name, description, dueDate);
+            taskList.Add(task);
+        }
+
+    }
+}
