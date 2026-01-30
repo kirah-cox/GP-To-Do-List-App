7c5c6eb0b29ceff610a9f9a9959d74c01a737677
kirah.cox <kirahanncox@gmail.com>
2026-01-30 11:07:31 -0700

    Refactored code

--- GP To-Do List App/Program.cs
+++ GP To-Do List App/Program.cs
@@ -13,92 +13,125 @@ public class Program
         
         while (runProgram)
         {
-            Console.WriteLine("Welcome to your To-Do List! Select an option:");
-            Console.WriteLine("1: Add a Task");
-            Console.WriteLine("2: Remove a Task");
-            Console.WriteLine("3: Mark a Task as Complete");
-            Console.WriteLine("4: View Tasks");
-            Console.WriteLine("5: Exit");
-
+            WriteMenu();
 
             if (int.TryParse(Console.ReadLine(), out int option))
             {
-                if (option == 1)
+                switch (option)
                 {
-                    Console.WriteLine("What is the task name?");
-                    string taskName = Console.ReadLine();
-
-                    Console.WriteLine("What is the task description?");
-                    string taskDescription = Console.ReadLine();
-
-                    Console.WriteLine("When is the task due?");
-                    string dueDate = Console.ReadLine();
-
-                    toDoList.AddTask(taskName, taskDescription, dueDate);
-
-                    Console.WriteLine("The task has been added to the To-do list.\n");
-                }
-                else if (option == 2)
-                {
-                    if (toDoList.taskList.Count != 0)
-                    {
-                        Console.WriteLine("What task do you want to remove?");
-                        string taskName = Console.ReadLine();
-
-                        toDoList.RemoveTask(taskName);
-
-                        Console.WriteLine("The task has been removed from to the To-do list.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have no tasks to remove.\n");
-                    }
-                }
-                else if (option == 3)
-                {
-                    if (toDoList.taskList.Count != 0)
-                    {
-                        Console.WriteLine("What task do you want to mark as complete?");
-                        string taskName = Console.ReadLine();
-
-                        toDoList.MarkAsComplete(taskName);
-
-                        Console.WriteLine("This task has been marked as complete.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have no tasks to mark as complete.\n");
-                    }
-                }
-                else if (option == 4)
-                {
-                    if (toDoList.taskList.Count != 0)
-                    {
-                        Console.WriteLine("Here is your complete To-Do list:\n");
-
-                        toDoList.ViewList();
-
-                        Console.WriteLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Your To-Do list is empty.\n");
-                    }
-                }
-                else if (option == 5)
-                {
-                    Console.WriteLine("Thank you for using this program!");
-                    runProgram = false;
-                }
-                else
-                {
-                    Console.WriteLine("That is not a valid option.\n");
+                    case 1:
+                        ActionOne(toDoList);
+                        break;
+                    case 2:
+                        ActionTwo(toDoList);
+                        break;
+                    case 3:
+                        ActionThree(toDoList);
+                        break;
+                    case 4:
+                        ActionFour(toDoList);
+                        break;
+                    case 5:
+                        runProgram = ActionFive();
+                        break;
+                    default:
+                        InvalidOption();
+                        break;
                 }
             }
             else
             {
-                Console.WriteLine("That is not a valid option.\n");
+                InvalidOption();
             }
         }
+    }
+
+    private static void WriteMenu()
+    {
+        Console.WriteLine("Welcome to your To-Do List! Select an option:");
+        Console.WriteLine("1: Add a Task");
+        Console.WriteLine("2: Remove a Task");
+        Console.WriteLine("3: Mark a Task as Complete");
+        Console.WriteLine("4: View Tasks");
+        Console.WriteLine("5: Exit");
+    }
+
+    private static void InvalidOption()
+    {
+        Console.WriteLine("That is not a valid option.\n");
+    }
+
+    private static bool ActionFive()
+    {
+        bool runProgram;
+        Console.WriteLine("Thank you for using this program!");
+        runProgram = false;
+        return runProgram;
+    }
+
+    private static void ActionFour(ToDoList toDoList)
+    {
+        if (toDoList.taskList.Count != 0)
+        {
+            Console.WriteLine("Here is your complete To-Do list:\n");
+
+            toDoList.ViewList();
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Your To-Do list is empty.\n");
+        }
+    }
+
+    private static void ActionThree(ToDoList toDoList)
+    {
+        if (toDoList.taskList.Count != 0)
+        {
+            Console.WriteLine("What task do you want to mark as complete?");
+            string taskName3 = Console.ReadLine();
+
+            toDoList.MarkAsComplete(taskName3);
+
+            Console.WriteLine("This task has been marked as complete.\n");
+        }
+        else
+        {
+            Console.WriteLine("You have no tasks to mark as complete.\n");
+        }
+    }
+
+    private static void ActionTwo(ToDoList toDoList)
+    {
+        if (toDoList.taskList.Count != 0)
+        {
+            Console.WriteLine("What task do you want to remove?");
+            string taskName2 = Console.ReadLine();
+
+            toDoList.RemoveTask(taskName2);
+
+            Console.WriteLine("The task has been removed from to the To-do list.\n");
+        }
+        else
+        {
+            Console.WriteLine("You have no tasks to remove.\n");
+        }
+    }
+
+    private static void ActionOne(ToDoList toDoList)
+    {
+        Console.WriteLine("What is the task name?");
+        string taskName1 = Console.ReadLine();
+
+        Console.WriteLine("What is the task description?");
+        string taskDescription = Console.ReadLine();
+
+        Console.WriteLine("When is the task due?");
+        string dueDate = Console.ReadLine();
+
+        toDoList.AddTask(taskName1, taskDescription, dueDate);
+
+        Console.WriteLine("The task has been added to the To-do list.\n");
     }
 }
